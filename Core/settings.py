df89cf0fcaefeb6eb0c8acdcea46b1cfbed469b6
Yousef Osama <yo5739240@gmail.com>
2026-02-28 16:50:57 +0200

    first commit

--- Core/settings.py
+++ Core/settings.py
@@ -95,4 +95,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = '1youseff777@gmail.com'  
-EMAIL_HOST_PASSWORD = 'zxyv lbno ufjo bvvr'
+EMAIL_HOST_PASSWORD = 'zxyvlbnoufjobvvr'
